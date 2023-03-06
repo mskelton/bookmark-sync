8d9e292b652bf923d1d158db93029de309f0aecf
Mark Skelton <mdskelton99@gmail.com>
2023-03-06 11:05:49 -0600

    Update tab when active

--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,35 @@
-chrome.action.onClicked.addListener(async (tab) => {
-  if (!tab.url) return
+function findBookmark(tab: chrome.tabs.Tab) {
+  if (!tab.url) {
+    return []
+  }
 
   const { origin } = new URL(tab.url)
-  const results = await chrome.bookmarks.search({ query: origin })
+  return chrome.bookmarks.search({ query: origin })
+}
+
+async function updateIcon(tabId: number, tab: chrome.tabs.Tab) {
+  const results = await findBookmark(tab)
+  const icon =
+    results?.[0]?.url === tab.url ? "icon-48-active.png" : "icon-48.png"
+
+  await chrome.action.setIcon({ path: `./images/${icon}`, tabId })
+}
+
+chrome.tabs.onUpdated.addListener(async (tabId, _, tab) => {
+  updateIcon(tabId, tab)
+})
+chrome.tabs.onUpdated.addListener(async (tabId, _, tab) => {
+  updateIcon(tabId, tab)
+})
+
+chrome.action.onClicked.addListener(async (tab) => {
+  const results = await findBookmark(tab)
 
   if (results.length === 1) {
     await chrome.bookmarks.update(results[0].id, { url: tab.url })
+    await chrome.action.setIcon({
+      path: `./images/icon-48-active.png`,
+      tabId: tab.id,
+    })
   }
 })
